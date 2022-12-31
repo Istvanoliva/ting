349107fd88910441108b16dcc536a6f74528f65c
Istvan <olivaistvan@gmail.com>
2022-12-30 23:50:32 -0300

    feat: (file_process) process()

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    if (path_file) in str(instance._data):
+        return
+
+    lists = txt_importer(path_file)
+
+    files = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(lists),
+        "linhas_do_arquivo": lists,
+    }
+
+    instance.enqueue(files)
+    print(files)
 
 
 def remove(instance):
